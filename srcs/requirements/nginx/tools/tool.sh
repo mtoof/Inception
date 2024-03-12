ee0cd31b204627268b9a3e4e4e80f725e7ab8622
mtoof <mojtaba.toof@gmail.com>
2024-03-12 18:10:59 +0200

    nginx conf file is ok

--- srcs/requirements/nginx/tools/tool.sh
+++ srcs/requirements/nginx/tools/tool.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/
+#!/bin/sh
 
 openssl req \
        -newkey rsa:2048 -nodes -keyout /etc/ssl/private/nginx-selfsigned.key \
-       -x509 -days 365 -out $CERTS_ \
+       -x509 -days 365 -out /etc/ssl/private/nginx-selfsigned.crt \
 	   -subj "/C=FI/ST=Uusimaa/L=Helsinki/O=Hive Helsinki/CN=mtoof.42.fr"
+
+nginx -g "daemon off;"
